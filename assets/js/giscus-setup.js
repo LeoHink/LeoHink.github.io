0cb3cb0837d069ca1d9ca14f7bee46a9ca9c1d9b
LeoHink <92932326+LeoHink@users.noreply.github.com>
2026-03-11 16:44:01 +0000

    Deploying to gh-pages from @ LeoHink/LeoHink.github.io@e92676fdb917c16b09b0b813111bdfaf29e4e42a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "LeoHink/leo-hinckeldey",
+    "data-repo": "LeoHink/LeoHink.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
